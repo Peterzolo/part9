9c004812f8f11325f8e9b63541268961cb5274e0
Peter <petersolomon704@gmail.com>
2023-05-25 09:11:55 +0100

    part 9
    modified code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+app.use(express.json());
 
 const PORT = 5000;
 
