09e7fdd6b682fc9c6b8c79565fa8dd81add52991
Peter <petersolomon704@gmail.com>
2023-05-26 16:15:06 +0100

    Update helpers.ts
    modified code

--- src/bmi/utils/helpers.ts
+++ src/bmi/utils/helpers.ts
@@ -1,4 +1,4 @@
-import { IBodyMass } from "../interface";
+import { IBodyMass, IDailyExercise } from "../interface";
 
 export const calculateBmi = (bodyMass: IBodyMass): string => {
   const heightInMeters = bodyMass.height / 100;
@@ -14,3 +14,39 @@ export const calculateBmi = (bodyMass: IBodyMass): string => {
     return "Obese";
   }
 };
+
+export const calculateExercises = (
+  exerciseHours: number[],
+  target: number
+): IDailyExercise => {
+  const periodLength = exerciseHours.length;
+  const trainingDays = exerciseHours.filter((hours) => hours > 0).length;
+  const totalHours = exerciseHours.reduce((sum, hours) => sum + hours, 0);
+  const average = totalHours / periodLength;
+  const success = average >= target;
+
+  let rating = 1;
+  let ratingDescription = "Below target";
+
+  if (success) {
+    rating = 3;
+    ratingDescription = "Target achieved!";
+  } else if (average >= target * 0.8) {
+    rating = 2;
+    ratingDescription = "Close to target";
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+const exerciseHours = [3, 4, 2, 4.5, 3, 3, 1];
+const targetHours = 2.5;
+const result = calculateExercises(exerciseHours, targetHours);
