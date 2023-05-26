8c635904344cfd5df8305d04d99f7251f27956ed
Peter <petersolomon704@gmail.com>
2023-05-26 14:37:31 +0100

    Update exerciseCalculator.ts
    Modified code

--- src/bmi/exerciseCalculator.ts
+++ src/bmi/exerciseCalculator.ts
@@ -7,3 +7,41 @@ interface IDailyExercise {
   target: number;
   average: number;
 }
+
+const calculateExercises = (
+  exerciseHours: number[],
+  target: number
+): IDailyExercise => {
+  const periodLength = exerciseHours.length;
+  const trainingDays = exerciseHours.filter((hours) => hours > 0).length;
+  const totalHours = exerciseHours.reduce((sum, hours) => sum + hours, 0);
+  const average = totalHours / periodLength;
+  const success = average >= target;
+
+  let rating = 1;
+  let ratingDescription = "Below target";
+
+  if (success) {
+    rating = 3;
+    ratingDescription = "Target achieved!";
+  } else if (average >= target * 0.8) {
+    rating = 2;
+    ratingDescription = "Close to target";
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+// Example usage
+const exerciseHours = [3, 0, 2, 4.5, 0, 3, 1];
+const targetHours = 2.5;
+const result = calculateExercises(exerciseHours, targetHours);
+console.log(result);
