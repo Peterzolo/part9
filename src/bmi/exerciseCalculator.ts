74f8a682f808b8cfe9f5376ffb0fcd1da87b178c
Peter <petersolomon704@gmail.com>
2023-05-26 14:24:58 +0100

    Create exerciseCalculator.ts
    Added interface

---
+++ src/bmi/exerciseCalculator.ts
@@ -0,0 +1,9 @@
+interface IDailyExercise {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
