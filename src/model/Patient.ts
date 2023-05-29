257e7e719707242241496c1a0408e70caf8da1fc
Peter <petersolomon704@gmail.com>
2023-05-29 11:27:31 +0100

    Update Patient.ts
    Modified code

--- src/model/Patient.ts
+++ src/model/Patient.ts
@@ -1,32 +1,42 @@
 import mongoose, { Schema } from "mongoose";
 import { ICreatePatient } from "../interface/patient.interface";
 
-const PatientSchema: Schema<ICreatePatient> = new Schema<ICreatePatient>({
-  name: {
-    type: String,
-    required: true,
+const PatientSchema: Schema<ICreatePatient> = new Schema<ICreatePatient>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    occupation: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "Other"],
+      required: true,
+    },
+    healthRating: {
+      type: String,
+    },
+    dateOfBirth: {
+      type: String,
+      required: true,
+    },
+    ssn: {
+      type: String,
+      required: true,
+    },
   },
-  occupation: {
-    type: String,
-    required: true,
-  },
-  gender: {
-    type: String,
-    enum: ["male", "female", "Other"],
-    required: true,
-  },
-  healthRating: {
-    type: String,
-  },
-  dateOfBirth: {
-    type: String,
-    required: true,
-  },
-  ssn: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform: function (_doc, ret) {
+        delete ret.ssn;
+        return ret;
+      },
+    },
+  }
+);
 
 const PatientModel = mongoose.model<ICreatePatient>("Patient", PatientSchema);
 
